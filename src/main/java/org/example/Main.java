5afb29910dddaf61287f5101706a45d0e834fa8f
Inchang Woo <143403168+Kyle-Inc-Woo@users.noreply.github.com>
2024-11-03 00:26:07 -0700

    -- Update -- # Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,27 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        //1. test_BudgetManager
-        //BudgetManager budgetManager = new BudgetManager();
-        //budgetManager.insertBudget();
+
+        BudgetManager budgetManager = new BudgetManager();
+        ExpenseManager expenseManager = new ExpenseManager();
+        ForecastManager forecastManager = new ForecastManager();
+        EconomicIndicatorManager economicIndicatorManager = new EconomicIndicatorManager();
+
+        budgetManager.insertBudget(1, "Monthly Budget",
+                700.0, "2024-11-01", "2024-11-30");
+
+        expenseManager.addExpense(1, "Rent", 550.0, "2024-11-01");
+        expenseManager.addExpense(1, "Food", 15.0, "2024-11-02");
+
+        System.out.println("Expense Records: ");
+        expenseManager.viewExpenses(1).forEach(System.out::println);
+
+        double forecast = forecastManager.forecastNextMonthExpense(1);
+        System.out.println("Forecasted Expense for next month:  $" + forecast);
+
+        double adjustedBudget = economicIndicatorManager.adjustBudgetWithInflation(700.0);
+        System.out.println("Adjusted Budget with Inflation:  $" + adjustedBudget);
+
+        DataVisualizationManager.launch(DataVisualizationManager.class);
     }
 }
